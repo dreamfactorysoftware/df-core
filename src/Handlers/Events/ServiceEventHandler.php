595f564b7822aca894e3247c4078910e8bd46636
Lee Hicks <leehicks@dreamfactory.com>
2015-04-28 22:47:26 -0400

    handle request and response as arrays

--- src/Handlers/Events/ServiceEventHandler.php
+++ src/Handlers/Events/ServiceEventHandler.php
@@ -1,6 +1,9 @@
 <?php
 namespace DreamFactory\Rave\Handlers\Events;
 
+use DreamFactory\Library\Utility\ArrayUtils;
+use DreamFactory\Rave\Contracts\ServiceRequestInterface;
+use DreamFactory\Rave\Contracts\ServiceResponseInterface;
 use DreamFactory\Rave\Models\EventScript;
 use DreamFactory\Rave\Scripting\ScriptEngine;
 use Illuminate\Contracts\Events\Dispatcher;
@@ -8,6 +11,7 @@ use DreamFactory\Rave\Events\ResourcePreProcess;
 use DreamFactory\Rave\Events\ResourcePostProcess;
 use DreamFactory\Rave\Events\ServicePreProcess;
 use DreamFactory\Rave\Events\ServicePostProcess;
+use Illuminate\Contracts\Support\Arrayable;
 use \Log;
 
 class ServiceEventHandler
@@ -21,10 +25,10 @@ class ServiceEventHandler
      */
     public function onServicePreProcess( $event )
     {
-        $name = $event->service . '.' . strtolower($event->verb) . '.pre_process';
+        $name = $event->service . '.' . strtolower( $event->verb ) . '.pre_process';
         Log::debug( 'Service event: ' . $name );
 
-        return $this->handleEventScript($name, $event->request, null, $event->resource);
+        return $this->handleEventScript( $name, $event->request, null, $event->resource );
     }
 
     /**
@@ -36,10 +40,10 @@ class ServiceEventHandler
      */
     public function onServicePostProcess( $event )
     {
-        $name = $event->service . '.' . strtolower($event->verb) . '.post_process';
+        $name = $event->service . '.' . strtolower( $event->verb ) . '.post_process';
         Log::debug( 'Service event: ' . $name );
 
-        return $this->handleEventScript($name, $event->request, null, $event->resource);
+        return $this->handleEventScript( $name, $event->request, $event->response, $event->resource );
     }
 
     /**
@@ -51,10 +55,10 @@ class ServiceEventHandler
      */
     public function onResourcePreProcess( $event )
     {
-        $name = $event->service . '.' . $event->resourcePath . '.' . strtolower($event->verb) . '.pre_process';
+        $name = $event->service . '.' . $event->resourcePath . '.' . strtolower( $event->verb ) . '.pre_process';
         Log::debug( 'Resource event: ' . $name );
 
-        return $this->handleEventScript($name, $event->request, null, $event->resource);
+        return $this->handleEventScript( $name, $event->request, null, $event->resource );
     }
 
     /**
@@ -66,10 +70,10 @@ class ServiceEventHandler
      */
     public function onResourcePostProcess( $event )
     {
-        $name = $event->service . '.' . $event->resourcePath . '.' . strtolower($event->verb) . '.post_process';
+        $name = $event->service . '.' . $event->resourcePath . '.' . strtolower( $event->verb ) . '.post_process';
         Log::debug( 'Resource event: ' . $name );
 
-        return $this->handleEventScript($name, $event->request, null, $event->resource);
+        return $this->handleEventScript( $name, $event->request, $event->response, $event->resource );
     }
 
     /**
@@ -87,17 +91,73 @@ class ServiceEventHandler
         $events->listen( 'DreamFactory\Rave\Events\ResourcePostProcess', 'DreamFactory\Rave\Handlers\Events\ServiceEventHandler@onResourcePostProcess' );
     }
 
-    protected function handleEventScript( $name, $request, $response = null, $resource = null)
+    protected function handleEventScript( $name, $request, $response = null, $resource = null )
     {
         $model = EventScript::whereName( $name )->first();
-        if (!empty($model))
+        if ( !empty( $model ) )
         {
             $output = null;
-            ScriptEngine::runScript($model->content, $name, $model->getEngineAttribute(), $model->config, $request, $output);
+            $data = [ 'request' => static::serviceRequestToArray( $request ), 'resource' => $resource ];
+            if ( !is_null( $response ) )
+            {
+                $data['response'] = static::serviceResponseToArray( $response );
+            }
+
+            $result = ScriptEngine::runScript( $model->content, $name, $model->getEngineAttribute(), ArrayUtils::clean( $model->config ), $data, $output );
 
             return $output;
         }
 
         return null;
+    }
+
+    protected static function serviceRequestToArray( $request )
+    {
+        if ( $request instanceof ServiceRequestInterface )
+        {
+            return [
+                'api_version' => $request->getApiVersion(),
+                'method'      => $request->getMethod(),
+                'parameters'  => $request->query(),
+                'headers'     => $request->getHeader(),
+                'payload'     => $request->getPayloadData(),
+            ];
+        }
+
+        if ( is_array( $request ) )
+        {
+            return $request;
+        }
+
+        if ( $request instanceof Arrayable )
+        {
+            return $request->toArray();
+        }
+
+        return [ ];
+    }
+
+    protected static function serviceResponseToArray( $response )
+    {
+        if ( $response instanceof ServiceResponseInterface )
+        {
+            return [
+                'status_code'  => $response->getStatusCode(),
+                'content_type' => $response->getContentType(),
+                'content'      => $response->getContent(),
+            ];
+        }
+
+        if ( is_array( $response ) )
+        {
+            return $response;
+        }
+
+        if ( $response instanceof Arrayable )
+        {
+            return $response->toArray();
+        }
+
+        return [ ];
     }
 }

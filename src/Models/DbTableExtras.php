0aac45f7074262cacd8caec69fe3a2a092cc19d1
Lee Hicks <leehicks@dreamfactory.com>
2015-08-18 09:56:02 -0400

    schema extras work

--- src/Models/DbTableExtras.php
+++ src/Models/DbTableExtras.php
@@ -14,10 +14,13 @@ namespace DreamFactory\Core\Models;
  * @property string  $model
  * @method static \Illuminate\Database\Query\Builder|DbTableExtras whereId($value)
  * @method static \Illuminate\Database\Query\Builder|DbTableExtras whereServiceId($value)
+ * @method static \Illuminate\Database\Query\Builder|DbTableExtras whereTable($value)
  */
 class DbTableExtras extends BaseSystemModel
 {
     protected $table = 'db_table_extras';
+
+    protected $fillable = ['service_id','table','label','plural','name_field','description','model'];
 
     protected $casts = ['id' => 'integer', 'service_id' => 'integer'];
 }

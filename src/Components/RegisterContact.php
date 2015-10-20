76dc9f40cc112ae19bb5403cf43e4d034f455ea1
Lee Hicks <leehicks@dreamfactory.com>
2015-10-20 13:05:04 -0400

    update contact info

--- src/Components/RegisterContact.php
+++ src/Components/RegisterContact.php
@@ -21,30 +21,31 @@ class RegisterContact
      */
     public static function registerUser($user, $payload = [])
     {
-        $leadsource = 'product-install';
-        if (!config('df.standalone')){
-            if (false === strpos(gethostname(), '.enterprise.dreamfactory.com')){
+        $leadsource = 'Product Install DreamFactory';
+        if (!config('df.standalone')) {
+            if (false === strpos(gethostname(), '.enterprise.dreamfactory.com')) {
                 return true; // bail, not tracking
             }
-            $leadsource = 'website-freehosted';
+            $leadsource = 'Website Free Hosted';
         }
         $partner = env('DF_INSTALL', '');
         if (empty($partner) && (false !== stripos(env('DB_DATABASE', ''), 'bitnami'))) {
-            $partner = 'bitnami';
+            $partner = 'Bitnami';
         }
         $payload = array_merge(
             [
                 //	Requirements
-                'user_id'             => 5, // required for access, for now
-                'email'               => $user->email,
-                'name'                => $user->name,
-                'first_name'          => $user->first_name,
-                'last_name'           => $user->last_name,
+                'user_id'    => 5, // required for access, for now
+                'email'      => $user->email,
+                'name'       => $user->name,
+                'first_name' => $user->first_name,
+                'last_name'  => $user->last_name,
+                'leadevent'  => $leadsource,
                 'leadsource' => $leadsource,
-                'partner' => $partner,
-                'product' => 'dreamfactory',
-                'version' => config('df.version', 'unknown'),
-                'hostos' => PHP_OS,
+                'partner'    => $partner,
+                'product'    => 'DreamFactory',
+                'version'    => config('df.version', 'unknown'),
+                'hostos'     => PHP_OS,
             ],
             ArrayUtils::clean($payload)
         );

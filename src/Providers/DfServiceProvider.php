efe9d2312a2391d1803cf54924a01e638b51bb46
Lee Hicks <leehicks@dreamfactory.com>
2016-04-21 15:29:22 -0400

    cleanup service provider

--- src/Providers/DfServiceProvider.php
+++ src/Providers/DfServiceProvider.php
@@ -1,45 +1,28 @@
 <?php
 namespace DreamFactory\Core\Providers;
 
-use DreamFactory\Core\Database\Connectors\IbmConnector;
-use DreamFactory\Core\Database\Connectors\OracleConnector;
-use DreamFactory\Core\Database\Connectors\SqlAnywhereConnector;
-use DreamFactory\Core\Database\Connectors\SQLiteConnector;
-use DreamFactory\Core\Database\IbmConnection;
-use DreamFactory\Core\Database\OracleConnection;
-use DreamFactory\Core\Database\SqlAnywhereConnection;
 use DreamFactory\Core\Handlers\Events\ServiceEventHandler;
-use Illuminate\Database\SQLiteConnection;
 use Illuminate\Support\ServiceProvider;
 
 class DfServiceProvider extends ServiceProvider
 {
     public function register()
     {
+        \App::register(DfCorsServiceProvider::class);
+
+        // If oauth required, add provider here.
+        if (class_exists('Laravel\Socialite\SocialiteServiceProvider')) {
+            \App::register('Laravel\Socialite\SocialiteServiceProvider');
+        }
+
         \Event::subscribe(new ServiceEventHandler());
 
         // Add our database drivers.
-        $this->app->resolving('db', function ($db) {
-            $db->extend('sqlite', function ($config) {
-                $connector  = new SQLiteConnector();
-                $connection = $connector->connect($config);
-                return new SQLiteConnection($connection, $config["database"], $config["prefix"], $config);
-            });
-            $db->extend('sqlanywhere', function ($config) {
-                $connector  = new SqlAnywhereConnector();
-                $connection = $connector->connect($config);
-                return new SqlAnywhereConnection($connection, $config["database"], $config["prefix"], $config);
-            });
-            $db->extend('oracle', function ($config) {
-                $connector  = new OracleConnector();
-                $connection = $connector->connect($config);
-                return new OracleConnection($connection, $config["database"], $config["prefix"], $config);
-            });
-            $db->extend('ibm', function ($config) {
-                $connector  = new IbmConnector();
-                $connection = $connector->connect($config);
-                return new IbmConnection($connection, $config["database"], $config["prefix"], $config);
-            });
-        });
+        \App::register(DfSqlDbServiceProvider::class);
+
+        // If mongo required, add provider here.
+        if (class_exists('DreamFactory\Core\MongoDb\ServiceProvider')) {
+            \App::register('DreamFactory\Core\MongoDb\ServiceProvider');
+        }
     }
 }

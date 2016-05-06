c7c705cdb01795b43f837774a6c3209641460363
Arif Islam <arif@dreamfactory.com>
2016-05-06 01:14:43 -0400

    adding event and platform object with platform.api support

--- src/Scripting/Engines/Python.php
+++ src/Scripting/Engines/Python.php
@@ -50,8 +50,50 @@ class Python extends ExecutedEngine
         $scriptLines = explode("\n", $script);
 
         $enrobedScript = <<<python
-event = $jsonEvent;
-platform = $jsonPlatform;
+import httplib, json;
+from bunch import bunchify, unbunchify;
+
+eventDict = $jsonEvent;
+platformDict = $jsonPlatform;
+
+event = bunchify(eventDict);
+platform = bunchify(platformDict);
+
+__host = event.request.headers.host[0];
+__headers = {
+    'x-dreamfactory-api-key':platform.session.api_key,
+    'x-dreamfactory-session-token':platform.session.session_token
+    };
+
+class Api:
+	def __init__(self, host, header):
+		self.host = host;
+		self.header = header;
+		self.conn = httplib.HTTPConnection(host);
+
+	def get(self, path):
+		return self.call('GET', path);
+
+	def post(self, path, payload=''):
+		return self.call('POST', path, payload);
+
+	def put(self, path, payload=''):
+		return self.call('PUT', path, payload);
+
+	def patch(self, path, payload=''):
+		return self.call('PATCH', path, payload);
+
+	def delete(self, path, payload=''):
+		return self.call('DELETE', path, payload);
+
+	def call(self, verb, path, payload=''):
+		self.conn.request(verb, path, payload, self.header);
+		response = self.conn.getresponse();
+		return response.read();
+		
+api = Api(__host, __headers);
+
+platform.api = api;
 
 try:
     def my_closure(event, platform):
@@ -62,12 +104,12 @@ python;
 
         $enrobedScript .= <<<python
 
-    event['script_result'] =  my_closure(event, platform);
+    event.script_result =  my_closure(event, platform);
 except Exception as e:
-    event['script_result'] = {'error':str(e)};
-    event['exception'] = str(e)
+    event.script_result = {'error':str(e)};
+    event.exception = str(e)
 
-print event;
+print unbunchify(event);
 python;
         $enrobedScript = trim($enrobedScript);
 

b5cfda5d6659b1300c9f533b737aaa9c36590e93
Arif Islam <arif@dreamfactory.com>
2016-08-10 15:02:06 -0400

    workaround for v8js segfault issue

--- src/Scripting/Engines/V8Js.php
+++ src/Scripting/Engines/V8Js.php
@@ -1,14 +1,20 @@
 <?php
 namespace DreamFactory\Core\Scripting\Engines;
 
+use DreamFactory\Core\Contracts\ServiceResponseInterface;
 use DreamFactory\Core\Exceptions\InternalServerErrorException;
 use DreamFactory\Core\Exceptions\ServiceUnavailableException;
 use DreamFactory\Core\Scripting\BaseEngineAdapter;
 use DreamFactory\Library\Utility\Scalar;
 use DreamFactory\Core\Scripting\ScriptSession;
 use DreamFactory\Core\Utility\Session;
+use DreamFactory\Core\Enums\DataFormats;
+use DreamFactory\Core\Scripting\ScriptServiceRequest;
+use DreamFactory\Core\Enums\ServiceRequestorTypes;
+use DreamFactory\Core\Utility\ResponseFactory;
 use \Log;
 use \Config;
+use ServiceManager;
 
 /**
  * Plugin for the php-v8js extension which exposes the V8 Javascript engine
@@ -127,22 +133,10 @@ class V8Js extends BaseEngineAdapter
 
     public static function buildPlatformAccess($identifier)
     {
-        /**
-         * For some mysterious reason the v8 library produces segmentation fault for PHP 7
-         * when $session ($session is an array) is used directly below. However,
-         * when $session is re-constructed into a $newSession variable using the
-         * code below it magically works!
-         */
-        $session = Session::all();
-        $newSession = [];
-        foreach ($session as $key => $value){
-            $newSession[$key] = $value;
-        }
-
         return [
             'api'     => static::getExposedApi(),
             'config'  => Config::get('df'),
-            'session' => $newSession,
+            'session' => $session = Session::all(),
             'store'   => new ScriptSession(Config::get("script.$identifier.store"), app('cache'))
         ];
     }
@@ -214,6 +208,104 @@ class V8Js extends BaseEngineAdapter
         }
 
         return null;
+    }
+
+    /**
+     * @param string $method
+     * @param string $path
+     * @param array  $payload
+     * @param array  $curlOptions Additional CURL options for external requests
+     *
+     * @return array
+     */
+    public static function inlineRequest($method, $path, $payload = null, $curlOptions = [])
+    {
+        if (null === $payload || 'null' == $payload) {
+            $payload = [];
+        }
+
+        try {
+            if ('https:/' == ($protocol = substr($path, 0, 7)) || 'http://' == $protocol) {
+                $result = static::externalRequest($method, $path, $payload, $curlOptions);
+            } else {
+                $result = null;
+                $params = [];
+                if (false !== $pos = strpos($path, '?')) {
+                    $paramString = substr($path, $pos + 1);
+                    if (!empty($paramString)) {
+                        $pArray = explode('&', $paramString);
+                        foreach ($pArray as $k => $p) {
+                            if (!empty($p)) {
+                                $tmp = explode('=', $p);
+                                $name = array_get($tmp, 0, $k);
+                                $value = array_get($tmp, 1);
+                                $params[$name] = urldecode($value);
+                            }
+                        }
+                    }
+                    $path = substr($path, 0, $pos);
+                }
+
+                if (false === ($pos = strpos($path, '/'))) {
+                    $serviceName = $path;
+                    $resource = null;
+                } else {
+                    $serviceName = substr($path, 0, $pos);
+                    $resource = substr($path, $pos + 1);
+
+                    //	Fix removal of trailing slashes from resource
+                    if (!empty($resource)) {
+                        if ((false === strpos($path, '?') && '/' === substr($path, strlen($path) - 1, 1)) ||
+                            ('/' === substr($path, strpos($path, '?') - 1, 1))
+                        ) {
+                            $resource .= '/';
+                        }
+                    }
+                }
+
+                if (empty($serviceName)) {
+                    return null;
+                }
+
+                $format = DataFormats::PHP_ARRAY;
+                if (!is_array($payload)) {
+                    $format = DataFormats::TEXT;
+                }
+
+                Session::checkServicePermission($method, $serviceName, $resource, ServiceRequestorTypes::SCRIPT);
+
+                $request = new ScriptServiceRequest($method, $params);
+                $request->setContent($payload, $format);
+
+                //  Now set the request object and go...
+                $service = ServiceManager::getService($serviceName);
+                $result = $service->handleRequest($request, $resource);
+            }
+        } catch (\Exception $ex) {
+            $result = ResponseFactory::create($ex);
+
+            Log::error('Exception: ' . $ex->getMessage(), ['response' => $result]);
+        }
+
+        /**
+         * For some mysterious reason the v8 library produces segmentation fault for PHP 7
+         * when $result or content of the $result object (ServiceResponse) is used directly below. However,
+         * when $result or content of the $result object is re-constructed into a array using the
+         * code below it magically works!
+         */
+        if (version_compare(PHP_VERSION, '7.0.0') >= 0) {
+            if ($result instanceof ServiceResponseInterface) {
+                $content = $result->getContent();
+                if (is_array($content)) {
+                    $content = static::reBuildArray($content);
+                }
+                $result->setContent($content);
+            } elseif (is_array($result)) {
+                $result = static::reBuildArray($result);
+            }
+        }
+
+        return ResponseFactory::sendScriptResponse($result);
     }
 
     /**
@@ -314,7 +406,15 @@ class V8Js extends BaseEngineAdapter
      */
     protected function enrobeScript($script, array &$data = [], array $platform = [])
     {
-//        $this->engine->event = $data;
+        /**
+         * For some mysterious reason the v8 library produces segmentation fault for PHP 7
+         * when $platform array is used directly below. However,
+         * when $platform array is re-constructed using the
+         * code below it magically works!
+         */
+        if (version_compare(PHP_VERSION, '7.0.0') >= 0) {
+            $platform = static::reBuildArray($platform);
+        }
         $this->engine->platform = $platform;
 
         $jsonEvent = json_encode($data, JSON_UNESCAPED_SLASHES);
@@ -378,5 +478,20 @@ JS;
     public static function __callStatic($name, $arguments)
     {
         return call_user_func_array(['\\V8Js', $name], $arguments);
+    }
+
+    public static function reBuildArray($array)
+    {
+        $newArray = [];
+
+        foreach ($array as $k => $v) {
+            if (is_array($v)) {
+                $newArray[$k] = static::reBuildArray($v);
+            } else {
+                $newArray[$k] = $v;
+            }
+        }
+
+        return $newArray;
     }
 }

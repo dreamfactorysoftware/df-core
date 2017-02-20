d32e73f2ea4bb8bf179dc1dcaf7c6a2af2e0652a
Charles Harmon <charles@harmonmediagroup.com>
2017-02-20 15:55:38 -0500

    fixing indents, etc.

--- src/Exceptions/TooManyRequestsException.php
+++ src/Exceptions/TooManyRequestsException.php
@@ -8,16 +8,16 @@ use Symfony\Component\HttpFoundation\Response;
  */
 class TooManyRequestsException extends RestException
 {
-	/**
-	 * Constructor.
-	 *
-	 * @param string  $message error message
-	 * @param integer $code    error code
-	 * @param mixed   $previous
-	 * @param mixed   $context Additional information for downstream consumers
-	 */
-	public function __construct( $message = null, $code = null, $previous = null, $context = null )
-	{
-        	parent::__construct(Response::HTTP_TOO_MANY_REQUESTS, $message, $code ?: Response::HTTP_TOO_MANY_REQUESTS, $previous, $context);
-	}
+    /**
+     * Constructor.
+     *
+     * @param string  $message error message
+     * @param integer $code    error code
+     * @param mixed   $previous
+     * @param mixed   $context Additional information for downstream consumers
+     */
+    public function __construct( $message = null, $code = null, $previous = null, $context = null )
+    {
+        parent::__construct(Response::HTTP_TOO_MANY_REQUESTS, $message, $code ?: Response::HTTP_TOO_MANY_REQUESTS, $previous, $context);
+    }
 }

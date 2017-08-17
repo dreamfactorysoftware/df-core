93c561bdf3a0dc3de5cc43b8414abfeb76c84545
Lee Hicks <leehicks@dreamfactory.com>
2017-08-16 22:45:20 -0400

    Restore service doc to service

--- src/Services/ServiceManager.php
+++ src/Services/ServiceManager.php
@@ -215,7 +215,7 @@ class ServiceManager
 
         return \Cache::rememberForever('service_mgr:' . $name, function () use ($name) {
             /** @var Service $service */
-            $service = Service::whereName($name)->first();
+            $service = Service::with('service_doc_by_service_id')->whereName($name)->first();
             if (empty($service)) {
                 throw new NotFoundException("Could not find a service for $name");
             }

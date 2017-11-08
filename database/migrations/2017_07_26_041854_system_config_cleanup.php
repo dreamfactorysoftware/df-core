d2b5f8229f9f8bdf4236b2fb7f97e0100eacc98c
Lee Hicks <leehicks@dreamfactory.com>
2017-11-08 15:03:05 -0500

    DF-1231 #resolve #comment Fix SQL Server migration from older versions

--- database/migrations/2017_07_26_041854_system_config_cleanup.php
+++ database/migrations/2017_07_26_041854_system_config_cleanup.php
@@ -14,6 +14,11 @@ class SystemConfigCleanup extends Migration
     public function up()
     {
         if (Schema::hasColumn('system_config', 'login_with_user_name')) {
+            if ('sqlsrv' == DB::connection()->getDriverName()) {
+                $defaultContraint = DB::selectOne("SELECT OBJECT_NAME([default_object_id]) AS name FROM SYS.COLUMNS WHERE [object_id] = OBJECT_ID('[dbo].[system_config]') AND [name] = 'login_with_user_name'");
+                DB::statement("ALTER TABLE [dbo].[system_config] DROP CONSTRAINT $defaultContraint->name");
+            }
+
             Schema::table('system_config', function (Blueprint $t) {
                 $t->dropColumn('login_with_user_name');
             });

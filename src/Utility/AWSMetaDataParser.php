5d2febcfd342439fdce1d905b9c33b1c6ffec9ca
Volodymyr Polishchuk <volodymyrpo@softwareplanet.uk.com>
2020-04-22 14:31:10 +0300

    DP-181 Investigate how DreamFactory can be on the AWS Marketplace
    - Extract public key to separate file
    - Rewrite tests

--- src/Utility/AWSMetaDataParser.php
+++ src/Utility/AWSMetaDataParser.php
@@ -2,37 +2,30 @@
 
 class AWSMetaDataParser {
 
+    /**
+     * AWS public certificate
+     * @see https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/instance-identity-documents.html#instance-identity-signature-verification-example
+     *
+     * @var string $awsPublicCertificate
+     */
     protected $awsPublicCertificate;
     protected $token;
     protected $tokenTimeToLive = 21600;
+    /**
+     * AWS Metadata Server URL
+     * @see https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/instancedata-data-retrieval.html
+     *
+     * @var string $metadataServer
+     */
     public $metadataServer = 'http://169.254.169.254';
 
     public function __construct()
     {
-        $this->awsPublicCertificate = <<<TXT
------BEGIN CERTIFICATE-----
-MIIC7TCCAq0CCQCWukjZ5V4aZzAJBgcqhkjOOAQDMFwxCzAJBgNVBAYTAlVTMRkw
-FwYDVQQIExBXYXNoaW5ndG9uIFN0YXRlMRAwDgYDVQQHEwdTZWF0dGxlMSAwHgYD
-VQQKExdBbWF6b24gV2ViIFNlcnZpY2VzIExMQzAeFw0xMjAxMDUxMjU2MTJaFw0z
-ODAxMDUxMjU2MTJaMFwxCzAJBgNVBAYTAlVTMRkwFwYDVQQIExBXYXNoaW5ndG9u
-IFN0YXRlMRAwDgYDVQQHEwdTZWF0dGxlMSAwHgYDVQQKExdBbWF6b24gV2ViIFNl
-cnZpY2VzIExMQzCCAbcwggEsBgcqhkjOOAQBMIIBHwKBgQCjkvcS2bb1VQ4yt/5e
-ih5OO6kK/n1Lzllr7D8ZwtQP8fOEpp5E2ng+D6Ud1Z1gYipr58Kj3nssSNpI6bX3
-VyIQzK7wLclnd/YozqNNmgIyZecN7EglK9ITHJLP+x8FtUpt3QbyYXJdmVMegN6P
-hviYt5JH/nYl4hh3Pa1HJdskgQIVALVJ3ER11+Ko4tP6nwvHwh6+ERYRAoGBAI1j
-k+tkqMVHuAFcvAGKocTgsjJem6/5qomzJuKDmbJNu9Qxw3rAotXau8Qe+MBcJl/U
-hhy1KHVpCGl9fueQ2s6IL0CaO/buycU1CiYQk40KNHCcHfNiZbdlx1E9rpUp7bnF
-lRa2v1ntMX3caRVDdbtPEWmdxSCYsYFDk4mZrOLBA4GEAAKBgEbmeve5f8LIE/Gf
-MNmP9CM5eovQOGx5ho8WqD+aTebs+k2tn92BBPqeZqpWRa5P/+jrdKml1qx4llHW
-MXrs3IgIb6+hUIB+S8dz8/mmO0bpr76RoZVCXYab2CZedFut7qc3WUH9+EUAH5mw
-vSeDCOUMYQR7R9LINYwouHIziqQYMAkGByqGSM44BAMDLwAwLAIUWXBlk40xTwSw
-7HX32MxXYruse9ACFBNGmdX2ZBrVNGrN9N2f6ROk0k9K
------END CERTIFICATE-----
-TXT;
+        $this->awsPublicCertificate = \File::get(__DIR__ . '/AWSMarketplace.pub');
     }
 
     /**
-     * @return string|null
+     * @return string|bool
      */
     public function getToken()
     {
@@ -43,11 +36,14 @@ TXT;
             ],
         ]);
         if (Curl::getLastHttpCode() != 200) {
-            return null;
+            return false;
         }
         return $this->token;
     }
 
+    /**
+     * @return string|bool
+     */
     public function getInstanceIdentity()
     {
         $token = $this->getToken();
@@ -57,13 +53,16 @@ TXT;
                 CURLOPT_HTTPHEADER => [ 'X-aws-ec2-metadata-token: ' . $token, ],
             ]);
             if (Curl::getLastHttpCode() != 200) {
-                return null;
+                return false;
             }
             return $instanceIdentity;
         }
-        return null;
+        return false;
     }
 
+    /**
+     * @return bool|string
+     */
     protected function getInstanceIdentityPKCS7()
     {
         $token = $this->getToken();
@@ -73,19 +72,24 @@ TXT;
                 CURLOPT_HTTPHEADER => ['X-aws-ec2-metadata-token: ' . $token,],
             ]);
             if (Curl::getLastHttpCode() != 200) {
-                return null;
+                return false;
             }
             return $pkcs7;
         }
-        return null;
+        return false;
     }
 
+    /**
+     * @return string
+     */
     private function getInstanceIdentitySignature()
     {
         return "-----BEGIN PKCS7-----\n" . $this->getInstanceIdentityPKCS7() . "\n-----END PKCS7-----";
     }
 
-
+    /**
+     * @return bool|string
+     */
     public function getVerifiedInstanceIdentity()
     {
         $document = $this->getInstanceIdentity();
@@ -93,9 +97,15 @@ TXT;
         if ($this->isVerifiedInstanceIdentity($document, $signature, $this->awsPublicCertificate)) {
             return $document;
         }
-        return null;
+        return false;
     }
 
+    /**
+     * @param string $document
+     * @param string $signature
+     * @param string $awsPublicCertificate
+     * @return bool
+     */
     public function isVerifiedInstanceIdentity($document, string $signature, string $awsPublicCertificate)
     {
         try {
@@ -120,7 +130,7 @@ TXT;
     }
 
     /**
-     * @return string|null
+     * @return string|bool
      */
     public function getProductCode()
     {
@@ -129,11 +139,11 @@ TXT;
             $instanceIdentity = json_decode($instanceIdentity);
             return $instanceIdentity->marketplaceProductCodes[0];
         }
-        return null;
+        return false;
     }
 
     /**
-     * @return string|null
+     * @return string|bool
      */
     public function getInstanceId()
     {
@@ -142,7 +152,7 @@ TXT;
             $instanceIdentity = json_decode($instanceIdentity);
             return $instanceIdentity->instanceId;
         }
-        return null;
+        return false;
     }
 
     /**
--- tests/src/Utility/AWSMetaDataParserTest.php
+++ tests/src/Utility/AWSMetaDataParserTest.php
@@ -5,42 +5,130 @@ namespace DreamFactory\Core\Utility;
 use PHPUnit\Framework\TestCase;
 
 /**
+ * This unit tests requires additional mock server, so all test is disabled
+ * To use this test, you must remove _ (underscore) from the method names of this class,
+ * change $metadataServer, $instanceId, $token, $instanceIdentity variables.
+ *
+ * <h2>Mock Metadata Server</h2>
+ *
+ * Mock server should implement 4 endpoints:
+ *
+ * <table>
+ *   <thead>
+ *     <tr>
+ *       <th>#</th>
+ *       <th>Path</th>
+ *       <th>Description</th>
+ *     </tr>
+ *   </thead>
+ *   <tr>
+ *     <td>1.</td>
+ *     <td>/latest/api/token</td>
+ *     <td>Used in IMDSv2. Must be added to all other queries in the X-aws-ec2-metadata-token-ttl-seconds header </td>
+ *   </tr>
+ *   <tr>
+ *     <td>2.</td>
+ *     <td>/latest/dynamic/instance-identity/signature</td>
+ *     <td>Data that can be used by other parties to verify its origin and authenticity</td>
+ *   </tr>
+ *   <tr>
+ *     <td>3.</td>
+ *     <td>/latest/dynamic/instance-identity/pkcs7</td>
+ *     <td>Used to verify the document's authenticity and content against the signature</td>
+ *   </tr>
+ *   <tr>
+ *     <td>4.</td>
+ *     <td>/latest/dynamic/instance-identity/document</td>
+ *     <td>JSON containing instance attributes, such as instance-id, private IP address, etc</td>
+ *   </tr>
+ * </table>
+ *
+ * <h2>Mock server example using NodeJS</h2>
+ *
+ * <code>
+ *
+ * 'use strict';
+ *
+ * const express = require('express');
+ *
+ * const PORT = process.env.PORT || 8080;
+ * const HOST = process.env.HOST || '0.0.0.0';
+ * const TOKEN = process.env.TOKEN || '<token>';
+ *
+ * const SIGNATURE = `<real signature>`;
+ *
+ * const PKCS7 = `<real pkcs7 document>`;
+ *
+ * const DOCUMENT = `{
+ *   "marketplaceProductCodes" : [ "<real product code>" ],
+ *   "instanceId" : "<real instance id>",
+ * }`;
+ *
+ * const app = express();
+ *
+ * app.put('/latest/api/token', (req, res) => {
+ *     if (!req.header('X-aws-ec2-metadata-token-ttl-seconds')) {
+ *          res.status(500).send('No ttl exists');
+ *     }
+ *     res.send(TOKEN);
+ * });
+ *
+ * app.use('*', (req, res, next) => {
+ *     console.log(req.header('X-aws-ec2-metadata-token'));
+ *     if (req.header('X-aws-ec2-metadata-token') === TOKEN) {
+ *         next();
+ *         return;
+ *     }
+ *     res.status(401).send('No token exists');
+ * });
+ *
+ * app.get('latest/dynamic/instance-identity/signature', (req, res) => {
+ *    res.send(SIGNATURE.toString());
+ * });
+ *
+ * app.get('/latest/dynamic/instance-identity/pkcs7', (req, res) => {
+ *     res.send(PKCS7.toString());
+ * });
+ *
+ * app.get('/latest/dynamic/instance-identity/document', (req, res) => {
+ *     res.send(DOCUMENT.toString());
+ * });
+ *
+ * app.listen(PORT, HOST);
+ * console.log(`Running on http://${HOST}:${PORT}`);
+ * </code>
+ *
  * @property string instanceIdentity
+ * @property string token
+ * @property string productCode
+ * @property string instanceId
  */
 class AWSMetaDataParserTest extends TestCase
 {
-    public $metadataServer = 'http://172.17.0.2:8080';
+    /**
+     * Mock AWS Metadata Server
+     *
+     * @var string $metadataServer
+     */
+    public $metadataServer = '';
 
     public function setUp()
     {
         $this->instanceIdentity = <<<TEXT
-{
-  "accountId" : "116434471183",
-  "architecture" : "x86_64",
-  "availabilityZone" : "us-east-2c",
-  "billingProducts" : null,
-  "devpayProductCodes" : null,
-  "marketplaceProductCodes" : [ "82g146env9v8ht47fwzh7yquu" ],
-  "imageId" : "ami-02bf3771838d602ac",
-  "instanceId" : "i-08f52968cacaaccfe",
-  "instanceType" : "t2.micro",
-  "kernelId" : null,
-  "pendingTime" : "2020-03-18T17:49:42Z",
-  "privateIp" : "172.31.42.34",
-  "ramdiskId" : null,
-  "region" : "us-east-2",
-  "version" : "2017-09-30"
-}
+<place here real instance identity>
 TEXT;
+        $this->token = '<place here real token>';
+        $this->productCode = '<place here real product code>';
+        $this->instanceId = '<place here real instance id>';
     }
 
-    public function testCreating()
+    public function _testCreating()
     {
         $metaDataParser = new AWSMetaDataParser();
         $this->assertNotEmpty($metaDataParser->getAWSPublicKey());
     }
 
-    public function testGetToken() {
+    public function _testGetToken() {
         $metaDataParser = new AWSMetaDataParser();
         $metaDataParser->metadataServer = $this->metadataServer;
 
@@ -48,15 +136,15 @@ TEXT;
         $this->assertNotEmpty($token);
     }
 
-    public function testGetToken__rightToken() {
+    public function _testGetToken__rightToken() {
         $metaDataParser = new AWSMetaDataParser();
         $metaDataParser->metadataServer = $this->metadataServer;
 
         $token = $metaDataParser->getToken();
-        $this->assertEquals('AQAAAH598_vCAYxU-bQ72s1E9a5_18gHUOAo4veTvVoS8PHN1yUHog==', $token);
+        $this->assertEquals($this->token, $token);
     }
 
-    public function testGetInstanceIdentity() {
+    public function _testGetInstanceIdentity() {
         $metaDataParser = new AWSMetaDataParser();
         $metaDataParser->metadataServer = $this->metadataServer;
 
@@ -64,15 +152,7 @@ TEXT;
         $this->assertNotEmpty($instanceIdentity);
     }
 
-    public function testGetInstanceIdentity__rightIdentity() {
-        $metaDataParser = new AWSMetaDataParser();
-        $metaDataParser->metadataServer = $this->metadataServer;
-
-        $instanceIdentity = $metaDataParser->getInstanceIdentity();
-        $this->assertEquals($this->instanceIdentity, $instanceIdentity);
-    }
-
-    public function testGetVerifiedInstanceIdentity() {
+    public function _testGetVerifiedInstanceIdentity() {
         $metaDataParser = new AWSMetaDataParser();
         $metaDataParser->metadataServer = $this->metadataServer;
 
@@ -80,7 +160,7 @@ TEXT;
         $this->assertNotEmpty($instanceIdentity);
     }
 
-    public function testProductCodes() {
+    public function _testProductCodes() {
         $metaDataParser = new AWSMetaDataParser();
         $metaDataParser->metadataServer = $this->metadataServer;
 
@@ -88,16 +168,16 @@ TEXT;
         $this->assertNotEmpty($productCode);
     }
 
-    public function testProductCodes__rightCode() {
+    public function _testProductCodes__rightCode() {
         $metaDataParser = new AWSMetaDataParser();
         $metaDataParser->metadataServer = $this->metadataServer;
 
         $productCode = $metaDataParser->getProductCode();
-        $this->assertEquals('82g146env9v8ht47fwzh7yquu', $productCode);
+        $this->assertEquals($this->productCode, $productCode);
     }
 
 
-    public function testInstanceId() {
+    public function _testInstanceId() {
         $metaDataParser = new AWSMetaDataParser();
         $metaDataParser->metadataServer = $this->metadataServer;
 
@@ -105,12 +185,12 @@ TEXT;
         $this->assertNotEmpty($instanceId);
     }
 
-    public function testInstanceId__rightInstance() {
+    public function _testInstanceId__rightInstance() {
         $metaDataParser = new AWSMetaDataParser();
         $metaDataParser->metadataServer = $this->metadataServer;
 
         $instanceId = $metaDataParser->getInstanceId();
-        $this->assertEquals('i-08f52968cacaaccfe', $instanceId);
+        $this->assertEquals($this->instanceId, $instanceId);
     }
 
 }
